7a54d8fa99399d73609024eb28936c9a2bd5f8d5
basmasabry <120141275+basmasabry@users.noreply.github.com>
2022-12-16 17:23:02 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,46 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace project_2
+namespace project_1
 {
     internal class Program
     {
-       
         static void Main(string[] args)
         {
-
-            Console.WriteLine("enter ur 1st num:");
+            Console.WriteLine("enter ur 1st name: ");
             int n1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("enter ur last num: ");
+            Console.WriteLine("enter ur last name: ");
             int n2 = int.Parse(Console.ReadLine());
-            for (int i = n1; i <= n2; i++)
+            int num, count, prime, temp;
+            if (n1 > n2)
             {
-                if (isperfect(i)) Console.WriteLine(i);
+                temp = n1;
+                n1 = n2;
+                n2 = temp;
+            }
+            for (num = n1; num <= n2; num++)
+            {
+                if (num == 1||num==0)
+                {
+                    if (num == 0) num += 2;
+                    else num += 1;
+                }
+                   
+
+                prime = 1;
+                for (count = 2; count < num; count++)
+                {
+                    if(num%count == 0)
+                    {
+                        prime = 0;
+                        break;
+                    }
+                   
+                }
+                if (prime == 1)
+                {
+                    Console.WriteLine(num);
+                   
+                }
             }
 
 
-            
 
 
 
-
-
-
-
-        }
-        static bool isperfect (int num)
-        {
-
-            int sum = 0;
-            for (int i = 1; i < num; i++)
-            {
-                if (num % i == 0) sum += i;
-            }if (sum == num) return true ;
-            return false;
 
         }
     }
